de8fc2165e6052059466306aa5bc22c1499bd537
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-07-11 12:28:44 +0600

    add and remove to bookmark done

--- app/Http/Controllers/API/BookmarkController.php
+++ app/Http/Controllers/API/BookmarkController.php
@@ -25,7 +25,7 @@ class BookmarkController extends Controller
             return sendResponse(true, 'Bookmarked question details fetched successfully', BookmarkedQuestionResource::collection($questions), Response::HTTP_OK);
         } catch (\Exception $e) {
             Log::error('Question List Error: ' . $e->getMessage());
-            return sendResponse(false, 'Failed to fetch bookmarked questions', null, Response::HTTP_INTERNAL_SERVER_ERROR);
+            return sendResponse(false, 'Failed to fetch bookmarked questions', $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
     public function bookmarkedQuizzes()
@@ -35,7 +35,50 @@ class BookmarkController extends Controller
             return sendResponse(true, 'Bookmarked quizzes fetched successfully', BookmarkedQuizResource::collection($quizzes), Response::HTTP_OK);
         } catch (\Exception $e) {
             Log::error('Question List Error: ' . $e->getMessage());
-            return sendResponse(false, 'Failed to fetch bookmarked quizzes', null, Response::HTTP_INTERNAL_SERVER_ERROR);
+            return sendResponse(false, 'Failed to fetch bookmarked quizzes', $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function addQuestionDetailsToBookmark($question_id)
+    {
+        try {
+            $this->bookmarkService->addQuestionDetailsToBookmark($question_id);
+            return sendResponse(true, 'Question added to bookmark successfully', null, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            Log::error('Question List Error: ' . $e->getMessage());
+            return sendResponse(false, 'Failed to add question to bookmark', $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function removeQuestionDetailsFromBookmark($question_id)
+    {
+        try {
+            $this->bookmarkService->removeQuestionDetailsFromBookmark($question_id);
+            return sendResponse(true, 'Question removed from bookmark successfully', null, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            Log::error('Question List Error: ' . $e->getMessage());
+            return sendResponse(false, 'Failed to remove question from bookmark', $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function addQuizToBookmark($quiz_id)
+    {
+        try {
+            $this->bookmarkService->addQuizToBookmark($quiz_id);
+            return sendResponse(true, 'Quiz added to bookmark successfully', null, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            Log::error('Question List Error: ' . $e->getMessage());
+            return sendResponse(false, 'Failed to add quiz to bookmark', $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+    public function removeQuizFromBookmark($quiz_id)
+    {
+        try {
+            $this->bookmarkService->removeQuizFromBookmark($quiz_id);
+            return sendResponse(true, 'Quiz removed from bookmark successfully', null, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            Log::error('Question List Error: ' . $e->getMessage());
+            return sendResponse(false, 'Failed to remove quiz from bookmark', $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
